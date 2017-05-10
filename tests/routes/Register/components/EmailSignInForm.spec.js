7d13dca02f59a7b4ca11cd2348c02da9f94ed6df
Nick Hoffman <njhoffman1982@gmail.com>
2017-05-09 21:39:34 -0500

    added missing dependency

--- tests/routes/Register/components/EmailSignInForm.spec.js
+++ tests/routes/Register/components/EmailSignInForm.spec.js
@@ -9,20 +9,20 @@ describe('(Routes: Register) Component: EmailSignUpForm', () => {
   const mockStore = configureStore();
 
   it('Should renders shallow', () => {
-    const getState = {
-      form: {
-        'register' : 'test'
-
-      }
-    }
-    const store = mockStore(getState);
-    const wrapper = shallow(<EmailSignUpForm store={store} />);
-    console.info(wrapper.debug());
-    console.info(wrapper.dive().debug());
-    console.info(wrapper.dive().dive().debug());
-    console.info(wrapper.dive().dive().dive().debug());
-    console.info(wrapper.dive().dive().dive().dive().debug());
-    console.info(wrapper.dive().dive().dive().dive().dive().debug());
+  //   const getState = {
+  //     form: {
+  //       'register' : 'test'
+  //
+  //     }
+  //   }
+    // const store = mockStore(getState);
+    // const wrapper = shallow(<EmailSignUpForm store={store} />);
+    // console.info(wrapper.debug());
+    // console.info(wrapper.dive().debug());
+    // console.info(wrapper.dive().dive().debug());
+    // console.info(wrapper.dive().dive().dive().debug());
+    // console.info(wrapper.dive().dive().dive().dive().debug());
+    // console.info(wrapper.dive().dive().dive().dive().dive().debug());
 
     // it('Should contain an email, password and password confirmation field', () => { });
     // it('Should contain a submit button and a reset password button', () => { });
